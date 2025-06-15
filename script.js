ca7b22eb7eb458db8c34ef9b7933358f4cd7c935
ASHWAGANDHUM <agq772@gmail.com>
2025-06-16 01:40:38 +0800

    Added modal shopping cart windows to all site pages.

--- script.js
+++
@@ -1,6 +0,0 @@
-const formEl = document.querySelector('.filter-form');
-const filterBtnEl = document.querySelector('.filter-button');
-
-filterBtnEl.addEventListener('click', () => {
-    formEl.classList.toggle('filter-form_show');
-});
